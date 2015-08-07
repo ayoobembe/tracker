75dc1aeab8b6bfef7f3507878ee6b8c9a37e2537
Ayo <ayo.obembe@gmail.com>
2015-08-07 19:05:57 +0100

    very slight modifications, nothing serious

--- projects/django_angular/security_monitor/views.py
+++ projects/django_angular/security_monitor/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import Quandl
 
-def index(request):
+def home(request):
     return HttpResponse("Hello, there. Welcome.")
