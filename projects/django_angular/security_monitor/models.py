0c3f5a6cde3a7ffeed00fc9082440e8a9b96d3ef
Ayo <ayo.obembe@gmail.com>
2015-08-07 16:46:00 +0100

    set up security_monitor app, able to display pages requested from app

---
+++ projects/django_angular/security_monitor/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+#from django_pandas.managers import DataFrameManager
+
+
+"""
+# Should be able to get by without model for the meantime.
+class Security(models.Model):
+    quandl_code = models.CharField(max_length=25)
+    date_begin = models.DateTimeField()
+    date_end = models.DateTimeField()
+    objects = DataFrameManager()
+"""
--- projects/django_angular/thinkster_django_angular_boilerplate/urls.py
+++ projects/django_angular/thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
 urlpatterns = patterns(
     '',
+
+    url(r'^monitor/', include('security_monitor.urls')),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
